245b13f520d3ba406caa7796bd6897d06a5fa92c
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2023-07-31 22:15:26 +0300

    add slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
 'use strict';
+import novelties from "../parts/novelties/novelties.js";
+//import baner from "../parts/baner/baner.js";
 
 document.addEventListener('DOMContentLoaded', () => {
+    novelties();
 })
