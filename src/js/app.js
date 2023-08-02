c3ff6851c8f2cc0c2e192b27533b350814deb88d
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2023-08-02 11:19:56 +0300

    Add : menu drop

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,5 @@ import novelties from "../parts/novelties/novelties.js";
 //import baner from "../parts/baner/baner.js";
 
 document.addEventListener('DOMContentLoaded', () => {
-    novelties();
+    // novelties();
 })
