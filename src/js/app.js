b155b1e9b14e9b49fdf5f0f17d45c06f833cbca2
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2023-08-03 03:16:23 +0300

    Add : cross

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 'use strict';
 import novelties from "../parts/novelties/novelties.js";
 //import baner from "../parts/baner/baner.js";
-
+import burger from "../parts/burger/burger.js";
 document.addEventListener('DOMContentLoaded', () => {
     // novelties();
+    burger();
 })
