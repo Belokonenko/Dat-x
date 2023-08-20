72ed9173ccdca9ad897df0b1072057f2fa9bff68
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2023-08-20 19:25:32 +0300

    Fix : top and burger

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 import novelties from "../parts/novelties/novelties.js";
 //import baner from "../parts/baner/baner.js";
-import burger from "../parts/burger/burger.js";
+import burger from "../parts/header/top/burger/burger.js";
 document.addEventListener('DOMContentLoaded', () => {
     // novelties();
     burger();
