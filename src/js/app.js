4fba42c78baaff8e21962e521a9061aea6858942
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2023-08-20 23:20:19 +0300

    Ref : parts

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-import novelties from "../parts/novelties/novelties.js";
+import novelties from "../parts/main/novelties/_novelties.js"
 //import baner from "../parts/baner/baner.js";
 import burger from "../parts/header/top/burger/burger.js";
 document.addEventListener('DOMContentLoaded', () => {
