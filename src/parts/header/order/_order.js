ea45b6791b1e158831107881d65b995ee75136e1
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-09-04 12:33:23 +0300

    Fix : Mob-header

--- src/parts/header/order/_order.js
+++ src/parts/header/order/_order.js
@@ -1,15 +1,20 @@
 export default function order() {
     const telBlock = document.querySelectorAll(".order__tel-block");
     const telBlockList = document.querySelectorAll(".order__list");
+    const orderTelRefs = document.querySelectorAll(".order__tel-ref");
 
     telBlock.forEach((item, i) => {
         item.addEventListener("mouseover", () => {
             telBlockList[i].classList.add("order__list--active");
-            console.log("mouseover");
         });
         item.addEventListener("mouseleave", () => {
             telBlockList[i].classList.remove("order__list--active");
-            console.log("mouseleave");
+        });
+    });
+
+    orderTelRefs.forEach((item) => {
+        item.addEventListener("click", ()=> {
+            telBlockList[i].classList.remove("order__list--active");
         });
     });
 }
