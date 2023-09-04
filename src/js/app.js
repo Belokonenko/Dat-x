0436a8f9f2b2d5a469c9348577137c82db89209f
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2023-09-04 19:52:11 +0300

    Fix : banner

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 'use strict';
 import novelties from "../parts/main/novelties/_novelties.js"
-//import baner from "../parts/baner/baner.js";
 import burger from "../parts/header/top/burger/burger.js";
 import order from "../parts/header/order/_order.js";
+import btn from "../parts/elements/btn/_btn.js";
+
 document.addEventListener('DOMContentLoaded', () => {
     // novelties();
     order();
     burger();
+    btn();
 })
