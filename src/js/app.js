8e72b8f1a520563eb36639a248499076df375bc0
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2023-09-07 20:30:23 +0300

    Ref : nav-prod

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,9 @@
 import novelties from "../parts/main/novelties/_novelties.js"
 import burger from "../parts/header/top/burger/burger.js";
 import order from "../parts/header/order/_order.js";
-import btn from "../parts/elements/btn/_btn.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     // novelties();
     order();
     burger();
-    btn();
 })
