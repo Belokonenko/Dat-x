2f2b1714956953a6c0f0f4c730667ac886c866ac
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2023-09-20 16:48:54 +0300

    Add : seeds btn

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,10 @@
 import burger from "../parts/header/top/burger/burger.js";
 import order from "../parts/header/order/_order.js";
 import  navProduct  from "../parts/header/nav-product/_nav-product.js";
+import seeds from "../parts/main/seeds/_seeds.js";
 document.addEventListener('DOMContentLoaded', () => {
     order();
     burger();
     navProduct();
+    seeds();
 })
