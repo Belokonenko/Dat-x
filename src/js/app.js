9e0216c85b88f7e33399bbf69d0e2c21f6a8178e
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2023-09-20 17:30:39 +0300

    Add : novelties add js carusel

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,12 @@ import burger from "../parts/header/top/burger/burger.js";
 import order from "../parts/header/order/_order.js";
 import  navProduct  from "../parts/header/nav-product/_nav-product.js";
 import seeds from "../parts/main/seeds/_seeds.js";
+import novelties from "../parts/main/novelties/_novelties.js";
+
 document.addEventListener('DOMContentLoaded', () => {
     order();
     burger();
     navProduct();
     seeds();
+    novelties();
 })
