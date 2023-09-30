bf95cab4ce6e32485be33a3fed62ddac33635218
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2023-09-30 03:42:28 +0300

    Fix : problev shift slide with gap

--- src/parts/main/novelties/_novelties.js
+++ src/parts/main/novelties/_novelties.js
@@ -1,140 +1,213 @@
 export default function novelties() {
-    const container = document.querySelector(".novelties__container");
-    const slider = document.querySelector(".novelties__slider");
-    const slides = document.querySelectorAll(".novelties__slide").length;
-    const buttons = document.querySelectorAll(".novelties__btn");
-    const butLeft = document.querySelector(".novelties__btn-left");
-    const butRight = document.querySelector(".novelties__btn-right");
+    const slider = document.querySelector(".slider");
+    const sliderLine = document.querySelector(".slider-line");
+    const sliderItems = document.querySelectorAll(".slider-item");
+    //------------------------------------------------------------
+    const bntLeft = document.querySelector(".bnt-left");
+    const bntRight = document.querySelector(".bnt-right");
 
-    let currentPosition = 0;
-    let currentMargin = 0;
-    let slidesPerPage = 0;
-    let slidesCount = slides - slidesPerPage;
-    let containerWidth = container.offsetWidth;
-    var prevKeyActive = false;
-    var nextKeyActive = true;
+    let counnt = 0;
+
+    // --- main ---
+
+    resetSize();
+
+    window.addEventListener("resize", resetSize);
+
+    bntLeft.addEventListener("click", () => {
+        left();
+    });
+
+    bntRight.addEventListener("click", () => {
+        right();
+    });
+
+    // --- main ---
+
+    // --- functions ---
+
+    function getWidthSlider() {
+        return slider.clientWidth;
+    }
+
+    function getWidthItem() {
+        return getWidthSlider() / getCounntVisebleItem();
+    }
+
+    function getCounntItems() {
+        return sliderItems.length;
+    }
+
+    function getMaxCounnt() {
+        return getCounntItems() - getCounntVisebleItem();
+    }
+
+    function resetSize() {
+        setWidthItems();
+        mouveLine();
+    }
+
+    function setWidthItems() {
+        const slideWidth = `${getWidthSlider() / getCounntVisebleItem()- calcGap() }px`;
+
+        sliderItems.forEach((item) => {
+            // mouve all slids and set size
+            item.style.width = slideWidth;
+        });
+    }
     
-    const debounce = (callback, timeout) => {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => callback(), timeout);
-    };
-
-    window.addEventListener("resize", checkWidth);
-
-    function checkWidth() {
-        console.log("checkWidth");
-        containerWidth = container.offsetWidth;
-        setParams(containerWidth);
-
+    function getGap() {
+        return parseInt(window.getComputedStyle(sliderLine).gap);
     }
 
-    function setParams(w) {
-        if (w < 551) {
-            slidesPerPage = 1;
-        } else {
-            if (w < 901) {
-                slidesPerPage = 2;
-            } else {
-                if (w < 1101) {
-                    slidesPerPage = 3;
-                } else {
-                    slidesPerPage = 4;
-                }
+    function calcGap() {
+        let gap = parseInt(window.getComputedStyle(sliderLine).gap);
+        console.log(gap);
+        console.log(gap * ( getCounntVisebleItem() - 1) /  getCounntVisebleItem());
+        return gap * ( getCounntVisebleItem() - 1) /  getCounntVisebleItem()
+    }
+
+    function getCounntVisebleItem() {
+        let counntVisebleItem = 1;
+        let width = getWidthSlider();
+
+        switch (true) {
+            case width >= 1440:
+                counntVisebleItem = 4;
+                break;
+            case width >= 1024:
+                counntVisebleItem = 4;
+                break;
+            case width >= 992:
+                counntVisebleItem = 3;
+                break;
+            case width >= 768:
+                counntVisebleItem = 3;
+                break;
+            case width >= 425:
+                counntVisebleItem = 3;
+                break;
+            case width >= 375:
+                counntVisebleItem = 2;
+                break;
+            case width >= 325:
+                counntVisebleItem = 1;
+                break;
+        }
+
+        return counntVisebleItem;
+    }
+
+    // --- mouve ---
+
+    function mouveLine() {
+        //------- no break point mouve -----------
+
+        console.log(`function mouveline : counnt = ${counnt}`);
+
+        if (counnt < 0) {
+            counnt = getMaxCounnt();
+        }
+
+        if (counnt > getMaxCounnt()) {
+            counnt = 0;
+        }
+
+        //------- /no break point mouve -----------
+        sliderLine.style.transform = `translateX(-${
+            (getWidthItem()+ getGap()/ getCounntVisebleItem()) * counnt
+        }px)`;
+    }
+
+    function left() {
+        --counnt;
+        mouveLine();
+    }
+
+    function right() {
+        ++counnt;
+        mouveLine();
+    }
+
+    // --- /mouve ---
+    //
+    // ---  swipe ---
+
+    let posInit = 0;
+    let posX1 = 0;
+    let posX2 = 0;
+    let posFinal = 0;
+
+    let trfRegExp = /[-0-9.]+(?=px)/;
+
+    slider.addEventListener("mousedown", swipeStart);
+    slider.addEventListener("touchstart", swipeStart);
+
+    function getEvent(event) {
+        // return evetn for touch or mouse to .clientX
+        return event.type.search("touch") !== -1 ? event.touches[0] : event;
+    }
+
+    function swipeStart(event) {
+        let e = getEvent(event);
+        posInit = posX1 = e.clientX; // first coordinate X axis
+
+        sliderLine.style.transition = ""; // remove the smooth transition
+
+        // отслеживать другие события на документе
+        slider.addEventListener("touchmove", swipeAction);
+        slider.addEventListener("touchend", swipeEnd);
+        slider.addEventListener("mousemove", swipeAction);
+        slider.addEventListener("mouseup", swipeEnd);
+        slider.addEventListener("mouseout", swipeEnd);
+        slider.addEventListener("pointerout", swipeEnd);
+    }
+
+    function swipeAction(event) {
+        let e = getEvent(event);
+        let style = sliderLine.style.transform; // получаем занчение transform в формате 'translateX(0px)'
+        let transform = +style.match(trfRegExp)[0]; // считываем трансформацию с помощью регулярного выражения и сразу превращаем в число
+
+        posX2 = posX1 - e.clientX; // получаем разницу start и mouve
+
+        posX1 = e.clientX; // текущее положение в x1
+
+        sliderLine.style.transform = `translateX(${transform - posX2}px)`; // двигаем sliderLine
+    }
+
+    function swipeEnd() {
+        console.log(`swipeEnd()`);
+        // финальная позиция курсора
+        posFinal = posInit - posX1;
+
+        slider.removeEventListener("touchmove", swipeAction);
+        slider.removeEventListener("mousemove", swipeAction);
+        slider.removeEventListener("touchend", swipeEnd);
+        slider.removeEventListener("mouseup", swipeEnd);
+        slider.removeEventListener("mouseout", swipeEnd);
+
+        slider.removeEventListener("pointerout", swipeEnd);
+
+        if (Math.abs(posFinal) > getMaxCounnt()) {
+            // убираем знак минус и сравниваем с порогом сдвига слайда
+            if (posInit < posX1) {
+                // если мы тянули вправо, то уменьшаем номер текущего слайда
+                --counnt;
+                // если мы тянули влево, то увеличиваем номер текущего слайда
+            } else if (posInit > posX1) {
+                ++counnt;
             }
         }
 
-        slidesCount = slides - slidesPerPage;
-
-        if (currentPosition > slidesCount) {
-            currentPosition -= slidesPerPage;
+        // если курсор двигался, то запускаем функцию переключения слайдов
+        if (posInit !== posX1) {
         }
 
-        currentMargin = -currentPosition * (100 / slidesPerPage);
-        slider.style.marginLeft = currentMargin + "%";
-
-        if (currentPosition > 0) {
-            buttons[0].classList.remove("inactive");
-        }
-        if (currentPosition < slidesCount) {
-            buttons[1].classList.remove("inactive");
-        }
-        if (currentPosition >= slidesCount) {
-            buttons[1].classList.add("inactive");
-        }
+        mouveLine();
     }
 
-    setParams();
+    // --- /swipe ---
 
-    function slideRight() {
-        if (currentPosition != 0) {
-            slider.style.marginLeft = currentMargin + 100 / slidesPerPage + "%";
-            currentMargin += 100 / slidesPerPage;
-            currentPosition--;
-        }
-
-        if (currentPosition === 0) {
-            buttons[0].classList.add("inactive");
-        }
-
-        if (currentPosition < slidesCount) {
-            buttons[1].classList.remove("inactive");
-        }
-    }
-
-    function slideLeft() {
-        if (currentPosition != slidesCount) {
-            slider.style.marginLeft = currentMargin - 100 / slidesPerPage + "%";
-            currentMargin -= 100 / slidesPerPage;
-            currentPosition++;
-        }
-
-        if (currentPosition == slidesCount) {
-            buttons[1].classList.add("inactive");
-        }
-
-        if (currentPosition > 0) {
-            buttons[0].classList.remove("inactive");
-        }
-    }
-
-    butLeft.addEventListener("click", () => {
-        slideRight();
-    });
-
-    butRight.addEventListener("click", () => {
-        slideLeft();
-    });
-
-    // ---  swipe slides ---
-
-    slider.addEventListener("touchstart", handleTouchStart, false);
-    slider.addEventListener("touchmove", handleTouchMove, false);
-
-    let x1 = null;
-
-    function handleTouchStart(event) {
-        x1 = event.touches[0].clientX;
-    }
-
-    let timeoutId;
-
-
-    function handleTouchMove(event) {
-        if (!x1) {
-            return false;
-        }
-
-        let x2 = event.touches[0].clientX;
-        let xDiff = x1 - x2;
-
-        debounce(() => {
-            console.log(xDiff);
-            console.log(slider);
-            if (xDiff < 0) {
-              slideRight()  ;
-            }
-        },500);
-    }
-
-    // --- /swipe slides ---
+    // --- /functions ---
+    //
 }
