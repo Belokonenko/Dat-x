a5f6554626e961e43e80f11f5550bacc14e01de1
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2023-10-26 01:53:50 +0300

    Fix : Carusel work regardless

--- src/parts/main/novelties/_novelties.js
+++ src/parts/main/novelties/_novelties.js
@@ -1,279 +1,293 @@
 export default function novelties() {
-    const slider = document.querySelector(".slider");
-    const sliderLine = document.querySelector(".slider__line");
-    
-    
+    const sliderBlocs = document.querySelectorAll(".slider__block");
 
-    const sliderItems = document.querySelectorAll(".slider__item");
-    //------------------------------------------------------------
-    const bntLeft = document.querySelector(".slider__btn-left");
-    const bntRight = document.querySelector(".slider__btn-right");
-    //------------------------------------------------------------
-    const dotsList = document.querySelector(".slider__dot-list");
-    //------------------------------------------------------------
-    let counnt = Math.round(getCounntItems() / 2);
+    if (sliderBlocs) {
+        for (let index = 0; index < sliderBlocs.length; index++) {
+            const element = sliderBlocs[index];
+            initSlider(element) ;
+            console.log(element);
+        }
+    } 
 
-    let n = 0;
+    function initSlider(sliderblock) {
+        
 
-    // --- main ---
+        const slider = sliderblock.querySelector(".slider");
 
-    createDots();
+        const sliderLine = sliderblock.querySelector(".slider__line");
 
-    resetSize();
+        const sliderItems = sliderblock.querySelectorAll(".slider__item");
+        //------------------------------------------------------------
+        const bntLeft = sliderblock.querySelector(".slider__btn-left");
+        const bntRight = sliderblock.querySelector(".slider__btn-right");
+        //------------------------------------------------------------
+        const dotsList = sliderblock.querySelector(".slider__dot-list");
+        //------------------------------------------------------------
+        let counnt = Math.round(getCounntItems() / 2);
 
-    window.addEventListener("resize", resetSize);
+        let n = 0;
 
-    bntLeft.addEventListener("click", () => {
-        left();
-    });
+        // --- main ---
 
-    bntRight.addEventListener("click", () => {
-        right();
-    });
+        createDots();
 
-    sliderItems.forEach((item, i) => {
-        item.addEventListener("click", () => {
-           console.log(i); 
+        resetSize();
+
+        window.addEventListener("resize", resetSize);
+
+        bntLeft.addEventListener("click", () => {
+            left();
         });
-    });
 
-    // --- main ---
-
-    // --- functions ---
-
-    function getWidthSlider() {
-        return slider.clientWidth;
-    }
-
-    function getWidthItem() {
-        return getWidthSlider() / getCounntVisebleItem();
-    }
-
-    function getCounntItems() {
-        return sliderItems.length;
-    }
-
-    function getMaxCounnt() {
-        return getCounntItems() - getCounntVisebleItem();
-    }
-
-    function resetSize() {
-        setWidthItems();
-        mouveLine();
-    }
-
-    function setWidthItems() {
-        const slideWidth = `${
-            getWidthSlider() / getCounntVisebleItem() - calcGap()
-        }px`;
-
-        sliderItems.forEach((item) => {
-            // mouve all slids and set size
-            item.style.width = slideWidth;
+        bntRight.addEventListener("click", () => {
+            right();
         });
-    }
 
-    function getGap() {
-        return parseInt(window.getComputedStyle(sliderLine).gap);
-    }
+        sliderItems.forEach((item, i) => {
+            item.addEventListener("click", () => {
+                console.log(i);
+            });
+        });
 
-    function calcGap() {
-        let gap = parseInt(window.getComputedStyle(sliderLine).gap);
-        return (gap * (getCounntVisebleItem() - 1)) / getCounntVisebleItem();
-    }
+        // --- main ---
 
-    function getCounntVisebleItem() {
-        let counntVisebleItem = 1;
-        let width = getWidthSlider();
+        // --- functions ---
 
-        switch (true) {
-            case width >= 1440:
-                counntVisebleItem = 4;
-                break;
-            case width >= 1024:
-                counntVisebleItem = 4;
-                break;
-            case width >= 992:
-                counntVisebleItem = 3;
-                break;
-            case width >= 768:
-                counntVisebleItem = 3;
-                break;
-            case width >= 425:
-                counntVisebleItem = 2;
-                break;
-            case width >= 375:
-                counntVisebleItem = 1;
-                break;
-            case width >= 325:
-                counntVisebleItem = 1;
-                break;
+        function getWidthSlider() {
+            return slider.clientWidth;
         }
 
-        return counntVisebleItem;
-    }
-
-    // --- mouve ---
-
-    function mouveLine() {
-        //------- no break point mouve -----------
-        console.log(counnt);
-
-        if (counnt < 0) {
-            counnt = getMaxCounnt();
+        function getWidthItem() {
+            return getWidthSlider() / getCounntVisebleItem();
         }
 
-        if (counnt > getMaxCounnt()) {
-            counnt = 0;
+        function getCounntItems() {
+            return sliderItems.length;
         }
 
-        //------- /no break point mouve -----------
-        sliderLine.style.transform = `translateX(-${
-            (getWidthItem() + getGap() / getCounntVisebleItem()) * counnt
-        }px)`;
-
-        console.log(counnt);
-        activeDot(counnt);
-
-        if (counnt) {
+        function getMaxCounnt() {
+            return getCounntItems() - getCounntVisebleItem();
         }
-    }
 
-    function left() {
-        --counnt;
-        mouveLine();
-    }
+        function resetSize() {
+            setWidthItems();
+            mouveLine();
+        }
 
-    function right() {
-        ++counnt;
-        mouveLine();
-    }
+        function setWidthItems() {
+            const slideWidth = `${
+                getWidthSlider() / getCounntVisebleItem() - calcGap()
+            }px`;
 
-    // --- /mouve ---
-    //
-    // ---  swipe ---
+            sliderItems.forEach((item) => {
+                // mouve all slids and set size
+                item.style.width = slideWidth;
+            });
+        }
 
-    let posInit = 0;
-    let posX1 = 0;
-    let posX2 = 0;
-    let posFinal = 0;
+        function getGap() {
+            return parseInt(window.getComputedStyle(sliderLine).gap);
+        }
 
-    let trfRegExp = /[-0-9.]+(?=px)/;
+        function calcGap() {
+            let gap = parseInt(window.getComputedStyle(sliderLine).gap);
+            return (
+                (gap * (getCounntVisebleItem() - 1)) / getCounntVisebleItem()
+            );
+        }
 
-    slider.addEventListener("mousedown", swipeStart);
-    slider.addEventListener("touchstart", swipeStart);
+        function getCounntVisebleItem() {
+            let counntVisebleItem = 1;
+            let width = getWidthSlider();
 
-    function getEvent(event) {
-        // return evetn for touch or mouse to .clientX
-        return event.type.search("touch") !== -1 ? event.touches[0] : event;
-    }
+            switch (true) {
+                case width >= 1440:
+                    counntVisebleItem = 4;
+                    break;
+                case width >= 1024:
+                    counntVisebleItem = 4;
+                    break;
+                case width >= 992:
+                    counntVisebleItem = 3;
+                    break;
+                case width >= 768:
+                    counntVisebleItem = 3;
+                    break;
+                case width >= 425:
+                    counntVisebleItem = 2;
+                    break;
+                case width >= 375:
+                    counntVisebleItem = 1;
+                    break;
+                case width >= 325:
+                    counntVisebleItem = 1;
+                    break;
+            }
 
-    function swipeStart(event) {
-        let e = getEvent(event);
-        posInit = posX1 = e.clientX; // first coordinate X axis
+            return counntVisebleItem;
+        }
 
-        sliderLine.style.transition = ""; // remove the smooth transition
+        // --- mouve ---
 
-        // отслеживать другие события на документе
-        slider.addEventListener("touchmove", swipeAction);
-        slider.addEventListener("touchend", swipeEnd);
-        slider.addEventListener("mousemove", swipeAction);
-        slider.addEventListener("mouseup", swipeEnd);
-        slider.addEventListener("mouseout", swipeEnd);
-        slider.addEventListener("pointerout", swipeEnd);
-    }
+        function mouveLine() {
+            //------- no break point mouve -----------
+            console.log(counnt);
 
-    function swipeAction(event) {
-        let e = getEvent(event);
-        let style = sliderLine.style.transform; // получаем занчение transform в формате 'translateX(0px)'
-        let transform = +style.match(trfRegExp)[0]; // считываем трансформацию с помощью регулярного выражения и сразу превращаем в число
+            if (counnt < 0) {
+                counnt = getMaxCounnt();
+            }
 
-        posX2 = posX1 - e.clientX; // получаем разницу start и mouve
+            if (counnt > getMaxCounnt()) {
+                counnt = 0;
+            }
 
-        posX1 = e.clientX; // текущее положение в x1
+            //------- /no break point mouve -----------
+            sliderLine.style.transform = `translateX(-${
+                (getWidthItem() + getGap() / getCounntVisebleItem()) * counnt
+            }px)`;
 
-        sliderLine.style.transform = `translateX(${transform - posX2}px)`; // двигаем sliderLine
-    }
+            console.log(counnt);
+            activeDot(counnt);
 
-    function swipeEnd() {
-        // финальная позиция курсора
-        posFinal = posInit - posX1;
-
-        slider.removeEventListener("touchmove", swipeAction);
-        slider.removeEventListener("mousemove", swipeAction);
-        slider.removeEventListener("touchend", swipeEnd);
-        slider.removeEventListener("mouseup", swipeEnd);
-        slider.removeEventListener("mouseout", swipeEnd);
-
-        slider.removeEventListener("pointerout", swipeEnd);
-
-        if (Math.abs(posFinal) > getMaxCounnt()) {
-            // убираем знак минус и сравниваем с порогом сдвига слайда
-            if (posInit < posX1) {
-                // если мы тянули вправо, то уменьшаем номер текущего слайда
-                --counnt;
-                // если мы тянули влево, то увеличиваем номер текущего слайда
-            } else if (posInit > posX1) {
-                ++counnt;
+            if (counnt) {
             }
         }
 
-        // если курсор двигался, то запускаем функцию переключения слайдов
-        if (posInit !== posX1) {
+        function left() {
+            --counnt;
+            mouveLine();
         }
 
-        mouveLine();
-    }
+        function right() {
+            ++counnt;
+            mouveLine();
+        }
 
-    // --- /swipe ---
+        // --- /mouve ---
+        //
+        // ---  swipe ---
 
-    // --- dots ---
+        let posInit = 0;
+        let posX1 = 0;
+        let posX2 = 0;
+        let posFinal = 0;
 
-    function createDots() {
-        for (let index = 0; index < sliderItems.length; index++) {
-            const dotWrap = document.createElement("li");
-            const dot = document.createElement("div");
+        let trfRegExp = /[-0-9.]+(?=px)/;
 
-            
+        slider.addEventListener("mousedown", swipeStart);
+        slider.addEventListener("touchstart", swipeStart);
 
-            
-            dotWrap.classList.add("slider__dot-wrap");
-            dot.classList.add("slider__dot");
-            
-            dotWrap.setAttribute("data-cunt", index);
-            
-            dotWrap.addEventListener("click", () => {
-                counnt = index;
-                activeDot(counnt);
-                mouveLine();
+        function getEvent(event) {
+            // return evetn for touch or mouse to .clientX
+            return event.type.search("touch") !== -1 ? event.touches[0] : event;
+        }
+
+        function swipeStart(event) {
+            let e = getEvent(event);
+            posInit = posX1 = e.clientX; // first coordinate X axis
+
+            sliderLine.style.transition = ""; // remove the smooth transition
+
+            // отслеживать другие события на документе
+            slider.addEventListener("touchmove", swipeAction);
+            slider.addEventListener("touchend", swipeEnd);
+            slider.addEventListener("mousemove", swipeAction);
+            slider.addEventListener("mouseup", swipeEnd);
+            slider.addEventListener("mouseout", swipeEnd);
+            slider.addEventListener("pointerout", swipeEnd);
+        }
+
+        function swipeAction(event) {
+            let e = getEvent(event);
+            let style = sliderLine.style.transform; // получаем занчение transform в формате 'translateX(0px)'
+            let transform = +style.match(trfRegExp)[0]; // считываем трансформацию с помощью регулярного выражения и сразу превращаем в число
+
+            posX2 = posX1 - e.clientX; // получаем разницу start и mouve
+
+            posX1 = e.clientX; // текущее положение в x1
+
+            sliderLine.style.transform = `translateX(${transform - posX2}px)`; // двигаем sliderLine
+        }
+
+        function swipeEnd() {
+            // финальная позиция курсора
+            posFinal = posInit - posX1;
+
+            slider.removeEventListener("touchmove", swipeAction);
+            slider.removeEventListener("mousemove", swipeAction);
+            slider.removeEventListener("touchend", swipeEnd);
+            slider.removeEventListener("mouseup", swipeEnd);
+            slider.removeEventListener("mouseout", swipeEnd);
+
+            slider.removeEventListener("pointerout", swipeEnd);
+
+            if (Math.abs(posFinal) > getMaxCounnt()) {
+                // убираем знак минус и сравниваем с порогом сдвига слайда
+                if (posInit < posX1) {
+                    // если мы тянули вправо, то уменьшаем номер текущего слайда
+                    --counnt;
+                    // если мы тянули влево, то увеличиваем номер текущего слайда
+                } else if (posInit > posX1) {
+                    ++counnt;
+                }
+            }
+
+            // если курсор двигался, то запускаем функцию переключения слайдов
+            if (posInit !== posX1) {
+            }
+
+            mouveLine();
+        }
+
+        // --- /swipe ---
+
+        // --- dots ---
+
+        function createDots() {
+            console.log(sliderblock);
+            for (let index = 0; index < sliderItems.length; index++) {
+                const dotWrap = document.createElement("li");
+                const dot = document.createElement("div");
+
+                dotWrap.classList.add("slider__dot-wrap");
+                dot.classList.add("slider__dot");
+
+                dotWrap.setAttribute("data-cunt", index);
+
+                dotWrap.addEventListener("click", () => {
+                    counnt = index;
+                    activeDot(counnt);
+                    mouveLine();
+                });
+
+
+                dotsList.append(dotWrap);
+                dotWrap.append(dot);
+            }
+
+            activeDot(counnt);
+        }
+
+        function activeDot(num) {
+            console.log(`activeDot`);
+
+            const dots = getDots();
+
+            dots.forEach((item) => {
+                item.classList.remove("slider__dot--active");
             });
 
-            
-            dotsList.append(dotWrap);
-            dotWrap.append(dot);
+            dots[num].classList.add("slider__dot--active");
         }
 
-        activeDot(counnt);
+        function getDots() {
+            return sliderblock.querySelectorAll(".slider__dot");
+        }
+
+        // --- dots ---
+
+        // --- /functions ---
+        //
+        //
     }
-
-    function activeDot(num) {
-        console.log(`activeDot`);
-
-        const dots = getDots();
-
-        dots.forEach((item) => {
-            item.classList.remove("slider__dot--active");
-        });
-
-        dots[num].classList.add("slider__dot--active");
-    }
-
-    function getDots() {
-        return document.querySelectorAll(".slider__dot");
-    }
-
-    // --- dots ---
-
-    // --- /functions ---
-    //
 }
